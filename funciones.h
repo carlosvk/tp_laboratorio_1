9323cb0ce7e4da71034ef1c782de0f158da8d8f2
carlosvk <carlosvieyrakeller@gmail.com>
2018-04-12 21:20:50 -0300

    Add files via upload

---
+++ funciones.h
@@ -0,0 +1,6 @@
+int sumar(int num1, int num2,int *resultado);
+int restar(int num1, int num2, int *resultado);
+int multiplicar(int num1,int num2,int *resultado);
+float dividir(int num1, int num2, int *resultado);
+int factor(int num1,int *resultado);
+int todas(int num1,int num2, int *resultado);
